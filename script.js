d491b7b955d6e6f9b9452e9bf7a4bf0998967a62
ksherm45 <77851772+ksherm45@users.noreply.github.com>
2021-11-04 16:54:32 +0100

    second update

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let canvas = document.querySelector('canvas')
-canvas.style.backgroundColor = '#34851c'
+// canvas.style.backgroundColor = '#34851c'     // ask josh if canvas color is having effect 
 let ctx = canvas.getContext('2d')
 canvas.style.border = '2px solid black';
 
 let title = document.querySelector('#startScreen')
-
+                                                            // look here for both start screen options
 let startScreen = new Image();
 startScreen.src = './images/BikerStartScreen.jpg'
+
+let gameOverScreen = document.querySelector('#endScreen')
 
 let endScreen = new Image();
 endScreen.src = './images/endgamescreen.jpg'
@@ -14,9 +16,15 @@ endScreen.src = './images/endgamescreen.jpg'
 let road = new Image();
 road.src = './images/road.png'
 
+let bus = new Image();
+bus.src = './images/bus.png'
+
+let car2 = new Image();
+car2.src = './images/car2.png'
+
 let car = new Image();
 car.src = './images/car.png'
-let carHeight = 145, carWidth = 120
+let carHeight = 165, carWidth = 165
 let carX = 250
 let carY = 30
 
@@ -33,25 +41,38 @@ let intervalId = 0;
 let gameOver = false; 
 let score = 0;
 let driving = true;
-let decCar = 2
+let decCar = 4
 
 
 let cars = [
     {x: carX, y: carY},
     {x: carX + 280, y: carY - 400},
-    {x: carX + 550, y: carY - 800}
+    {x: carX + 315, y: carY - 1200},
+    {x: carX + 550, y: carY - 2020},
+    {x: carX + 400, y: carY - 2800},
+]
+
+let cars2 =[
+
+    {x: carX + 550, y: carY - 900},
+    {x: carX + 70, y: carY - 1600},
+    {x: carX + 280, y: carY - 2400},
+    {x: carX + 80, y: carY - 3000}
+]
+
+let bus1 = [
+
+    {x: carX + 30, y: carY - 2400}
 ]
 
 function draw(){
 
-    
-// ctx.drawImage(startScreen, 0, 0)
-// canvas.style.display = 'block'              //ask josh bout start image
 
 startBtn.style.display = 'none';
 restartBtn.style.display = 'none'; 
-canvas.style.display = 'block'
+// canvas.style.display = 'block'
 title.style.display = 'none'
+gameOverScreen.style.display = 'none'
 
     ctx.drawImage(road, 0, 0)
     canvas.style.display = 'block'
@@ -60,25 +81,23 @@ title.style.display = 'none'
 // hide the start button during the game and show restart button after a collision
 // if gameover is true make a if else condition 
 
-
+// red car array aka car
 for(let i=0; i<cars.length; i++){
 ctx.drawImage(car, cars[i].x, cars[i].y, carWidth, carHeight)
 cars[i].y = cars[i].y + decCar
 
 if(cars[i].y - car.height > canvas.height ) {
-    cars[i].y = -250
+    cars[i].y = -2500
 
 
 }
 if(cars[i].y == canvas.height) {
     score++
-    console.log(score)
+
 }
 //collision logic
 
     if(bikerX + bikerWidth >= cars[i].x && bikerX <= cars[i].x + (carWidth) && (bikerY <= cars[i].y + carHeight && bikerY+bikerHeight >= cars[i].y)){
-        
-        console.log(cars)
 
      gameOver = true;
   }
@@ -91,6 +110,70 @@ if(bikerX < 200 || bikerX > 880){
     ctx.fillText(`Score: ${score}`, 30, canvas.height - 70 )
 
 }
+
+// car2 array aka yellow car
+
+for(let i=0; i<cars2.length; i++){
+    ctx.drawImage(car2, cars2[i].x, cars2[i].y, carWidth, carHeight)
+    cars2[i].y = cars2[i].y + decCar
+    
+    if(cars2[i].y - car.height > canvas.height ) {
+        cars2[i].y = -2500
+    
+    
+    }
+    if(cars2[i].y == canvas.height) {
+        score++
+    
+    
+        console.log(score)
+    }
+    //collision logic
+    
+        if(bikerX + bikerWidth >= cars2[i].x && bikerX <= cars2[i].x + (carWidth) && (bikerY <= cars2[i].y + carHeight && bikerY+bikerHeight >= cars2[i].y)){
+            
+            
+    
+         gameOver = true;
+      }
+    
+    if(bikerX < 200 || bikerX > 880){
+        gameOver = true;
+    }
+    
+    }
+// bus logic
+    for(let i=0; i<bus1.length; i++){
+        ctx.drawImage(bus, bus1[i].x, bus1[i].y, carWidth, carHeight + 250)
+        bus1[i].y = bus1[i].y + decCar
+        
+        if(bus1[i].y - car.height + 250 > canvas.height ) {
+            bus1[i].y = -2500
+        
+        
+        }
+        if(bus1[i].y == canvas.height) {
+            score++
+        
+            console.log(score)
+        }
+        //collision logic
+        
+            if(bikerX + bikerWidth >= bus1[i].x && bikerX <= bus1[i].x + (carWidth) && (bikerY <= bus1[i].y + carHeight && bikerY+bikerHeight >= bus1[i].y)){
+                
+                console.log(cars)
+        
+             gameOver = true;
+          }
+        
+        if(bikerX < 200 || bikerX > 880){
+            gameOver = true;
+        }
+        
+           
+        
+        }
+
 
 
 
@@ -112,12 +195,15 @@ if(driving) {
 
 function splash(){
 
-    ctx.drawImage(startScreen, 0, 0)
-    title.style.display = 'block' 
 
-    ctx.drawImage(startScreen, 0, 0)
-    canvas.style.display = 'block'
+    
 
+    // ctx.drawImage(startScreen, 0, 0)
+     title.style.display = 'block' 
+
+    // ctx.drawImage(startScreen, 0, 0)
+    // canvas.style.display = 'block'
+    gameOverScreen.style.display = 'none'
     canvas.style.display = 'none'
     restartBtn.style.display = 'none'
     startBtn.style.display = 'block'
@@ -126,19 +212,34 @@ function splash(){
 
 function endGame(){
 
-    gameOver = false;                        // ask josh top 3 lines
+    gameOver = false;   
     canvas.style.display = 'none'
     restartBtn.style.display = 'block'
     startBtn.style.display = 'none'
+    gameOverScreen.style.display = 'block'
 
     bikerX = 630, bikerY = canvas.height - bikerHeight - 15
 
     cars = [
-        {x: carX, y: carY},
-        {x: carX + 280, y: carY - 400},  // runs the same without the array & if crashes in the middle cant restart
-        {x: carX + 550, y: carY - 800}
+        {x: 250, y: 30},
+        {x: 250 + 280, y: 30 - 400},
+        {x: 250 + 315, y: 30 - 1200},
+        {x: 250 + 550, y: 30 - 2020},
+        {x: 250 + 400, y: 30 - 2800},
     ]
-  //score = 0;
+    
+    cars2 =[
+    
+        {x: 250 + 550, y: 30 - 900},
+        {x: 250 + 70, y: 30 - 1600},
+        {x: 250 + 280, y: 30 - 2400},
+        {x: 250 + 80, y: 30 - 3000}
+    ]
+    
+    bus1 = [
+    
+        {x: 250 + 30, y: 30 - 2400}
+    ]
    
 
 }
